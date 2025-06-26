25d7fd9c1c681a8ac2ff489708c01777ae6e0728
OMAR <rockstonestudios666@gmail.com>
2025-06-25 20:29:09 -0500

    Five Commit

--- src/app/components/Benefit.tsx
+++ src/app/components/Benefit.tsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Image from "next/image";
 
-interface BenefitProps {
-    icon: React.ReactNode;
+type BenefitProps = {
+    icon: string;
     header: string;
     text: string;
-}
+};
 
 export default function Benefit({ icon, header, text }: BenefitProps) {
     return (
         <div>
             <div>
-                <img src={icon} alt="" className="w-12 mb-4" />
+                {icon && (
+                    <Image
+                        src={icon}
+                        alt={header}
+                        width={48}
+                        height={48}
+                        className="mb-4"
+                    />
+                )}
             </div>
             <h3 className="font-semibold text-2xl mb-2">{header}</h3>
             <p>{text}</p>
